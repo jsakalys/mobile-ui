115550bb49af958d39ee993047cb3f1236ed02e3
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2016-03-16 17:54:01 -0700

    added menu header & menu items

--- js/script.js
+++ js/script.js
@@ -1,4 +1,51 @@
-document.addEventListener('DOMContentLoaded', function(){
+var menuItems = [
+	{
+		title: 'My Schedule and Bidding'
+	},
+	{
+		title: 'Safety'
+	},
+	{
+		title: 'Training'
+	},
+	{
+		title: 'Administration'
+	},
+	{
+		title: 'Catering'
+	},
+	{
+		title: 'Brand'
+	},
+	{
+		title: 'Hotels'
+	},
+	{
+		title: 'My Base'
+	},
+	{
+		title: 'Recognition'
+	},
+	{
+		title: 'My Leadership Team'
+	}
+]
 
-	
-})
+document.addEventListener('DOMContentLoaded', function(){
+	for (var i = 0; i < menuItems.length; i++) {
+		// Append menu items
+		document.getElementById('mainMenu').innerHTML += '<div class="menu-item">' + menuItems[i].title + '</div>'
+
+	}
+	// Show main menu on navi click
+	document.getElementById('menuButton').addEventListener('click', function(){
+		var menuPos = document.getElementById('mainMenu').style.left;
+		if (menuPos !== '0px') {
+			document.getElementById('mainContent').style.left = '100vw';
+			document.getElementById('mainMenu').style.left = '0px';
+		} else {
+			document.getElementById('mainContent').style.left = '0px';
+			document.getElementById('mainMenu').style.left = '-100vw';
+		};
+	});
+});
